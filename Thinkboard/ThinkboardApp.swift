df6853ee65db473c7b5120dc0958cb82c3184b3b
Ian Ellis <ianellis015@gmail.com>
2025-06-01 19:56:06 -0700

    InitialSwiftUI app setup for Thinkboard

---
+++ Thinkboard/ThinkboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThinkboardApp.swift
+//  Thinkboard
+//
+//  Created by Ian Ellis on 6/1/25.
+//
+
+import SwiftUI
+
+@main
+struct ThinkboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ThinkboardTests/ThinkboardTests.swift
@@ -0,0 +1,17 @@
+//
+//  ThinkboardTests.swift
+//  ThinkboardTests
+//
+//  Created by Ian Ellis on 6/1/25.
+//
+
+import Testing
+@testable import Thinkboard
+
+struct ThinkboardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
